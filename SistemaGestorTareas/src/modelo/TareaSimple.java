54eb86ab7b78af49c392330aac45afa02e05bb05
Faber-Gomez <f.garzon@unimayor.edu.co>
2026-09-08 08:17:22 -0500

    Update TareaSimple.java

--- SistemaGestorTareas/src/modelo/TareaSimple.java
+++ SistemaGestorTareas/src/modelo/TareaSimple.java
@@ -1,9 +1,5 @@
 package modelo;
 
-/**
- * Tarea individual sin subtareas asociadas.
- * Hereda todos los atributos de Tarea.
- */
 public class TareaSimple extends Tarea {
 
     public TareaSimple(String titulo, String descripcion, String prioridad) {
