db0a2548eecdaa7cf5d7fb101d8d9bf1f87fe7ae
YersonChilito00 <yerson.chilito@unimayor.edu.co>
2026-09-08 08:17:49 -0500

    Update Main.java

--- SistemaGestorTareas/src/Main.java
+++ SistemaGestorTareas/src/Main.java
@@ -3,10 +3,6 @@ import vista.VentanaPrincipal;
 
 import javax.swing.SwingUtilities;
 
-/**
- * Punto de entrada del programa. No contiene logica propia:
- * solo crea el proyecto y llama a la vista.
- */
 public class Main {
 
     public static void main(String[] args) {
