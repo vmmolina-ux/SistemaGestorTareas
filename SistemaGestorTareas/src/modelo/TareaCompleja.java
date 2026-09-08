e23133f159b6cfb6a0567d510f3c4c8f9cddb532
silvanabanana21 <silvanasalazar398@gmail.com>
2026-09-08 08:28:35 -0500

    Update TareaCompleja.java

--- SistemaGestorTareas/src/modelo/TareaCompleja.java
+++ SistemaGestorTareas/src/modelo/TareaCompleja.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Tarea que se compone de una lista de subtareas.
- */
+ * 
 public class TareaCompleja extends Tarea {
 
     private List<Tarea> subtareas;
